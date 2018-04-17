f483bf3e64ad95d3fb57cc9b6b86a57e07c93d4a
BI Yue <biyue111@126.com>
2018-04-17 20:45:06 +0800

    Update web_requests.py

--- broker/web_requests.py
+++ broker/web_requests.py
@@ -3,41 +3,34 @@ import urllib2
 
 import json 
 
+class web_request:
+        
+        def __init__(self):
+                pass
+        
+        def send_request(self, data, url, api_key):
 
-data =  {
+                body = str.encode(json.dumps(data))
 
-        "Inputs": {
+                #url = 'https://ussouthcentral.services.azureml.net/workspaces/0a46720be6f3493bb4c2c05f05e3e5c4/services/27176938495a45368fbb33bb7f26e1e3/execute?api-version=2.0&details=true'
+                #api_key = 'yuhAKrw329F26sq9vXkc6JPt2kpvW0jNZYocNdHL32z3a8yM1DlCyUcbT06FkllVND2PtghZWOucvu/1r5kgYw==' # Replace this with the API key for the web service
+                headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
 
-                "input2":
-                {
-                    "ColumnNames": ["temperature", "humidity"],
-                    "Values": [ [ "value", "value" ], [ "value", "value" ], ]
-                },        },
-            "GlobalParameters": {
-}
-    }
+                req = urllib2.Request(url, body, headers) 
 
-body = str.encode(json.dumps(data))
+                try:
+                    response = urllib2.urlopen(req)
 
-url = 'https://ussouthcentral.services.azureml.net/workspaces/0a46720be6f3493bb4c2c05f05e3e5c4/services/27176938495a45368fbb33bb7f26e1e3/execute?api-version=2.0&details=true'
-api_key = 'yuhAKrw329F26sq9vXkc6JPt2kpvW0jNZYocNdHL32z3a8yM1DlCyUcbT06FkllVND2PtghZWOucvu/1r5kgYw==' # Replace this with the API key for the web service
-headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+                    # If you are using Python 3+, replace urllib2 with urllib.request in the above code:
+                    # req = urllib.request.Request(url, body, headers) 
+                    # response = urllib.request.urlopen(req)
 
-req = urllib2.Request(url, body, headers) 
+                    result = response.read()
+                    print(result) 
+                except urllib2.HTTPError, error:
+                    print("The request failed with status code: " + str(error.code))
 
-try:
-    response = urllib2.urlopen(req)
+                    # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+                    print(error.info())
 
-    # If you are using Python 3+, replace urllib2 with urllib.request in the above code:
-    # req = urllib.request.Request(url, body, headers) 
-    # response = urllib.request.urlopen(req)
-
-    result = response.read()
-    print(result) 
-except urllib2.HTTPError, error:
-    print("The request failed with status code: " + str(error.code))
-
-    # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
-    print(error.info())
-
-    print(json.loads(error.read()))        
+                    print(json.loads(error.read()))        
