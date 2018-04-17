bdde57d2db9b2fc811bd70542006c8caebffd8d4
BI Yue <biyue111@126.com>
2018-04-17 20:45:27 +0800

    Update config.py

--- broker/config.py
+++ broker/config.py
@@ -1,2 +1,16 @@
+
+data =  {
+
+        "Inputs": {
+
+                "input2":
+                {
+                    "ColumnNames": ["temperature", "humidity"],
+                    "Values": [ [ "value", "value" ], [ "value", "value" ], ]
+                },        },
+            "GlobalParameters": {
+}
+    }
+
 web_url = 'https://ussouthcentral.services.azureml.net/workspaces/0a46720be6f3493bb4c2c05f05e3e5c4/services/27176938495a45368fbb33bb7f26e1e3/execute?api-version=2.0&details=true'
 api_key = 'yuhAKrw329F26sq9vXkc6JPt2kpvW0jNZYocNdHL32z3a8yM1DlCyUcbT06FkllVND2PtghZWOucvu/1r5kgYw=='
