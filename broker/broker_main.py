e2c4ddd6681f1832af18ab8bd31901f100dab337
BI Yue <biyue111@126.com>
2018-04-17 22:11:40 +0800

    Update broker_main.py

--- broker/broker_main.py
+++ broker/broker_main.py
@@ -1,6 +1,14 @@
 from web_requests import web_request
 import config_noIoT as config_noIoT
 
+def format_data(data, d, v):
+  ind = data["inputs"]["input2"]["ColumnNames"].index(d)
+  data["inputs"]["input2"]["Values"][ind] = v
+  
+  return data
+
+
 request = web_request()
-data = config_noIoT.data_format % (10, 50)
+data = format_data("temperature", 20)
+data = format_data("humidity", 20)
 request.send_request(data, config_noIoT.web_url, config_noIoT.api_key)
