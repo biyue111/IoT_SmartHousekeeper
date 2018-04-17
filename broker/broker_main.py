4b640ebfb4f638025fe257a993e2cdf8d7ad5185
BI Yue <biyue111@126.com>
2018-04-17 21:24:26 +0800

    Update broker_main.py

--- broker/broker_main.py
+++ broker/broker_main.py
@@ -1,4 +1,6 @@
 from web_requests import web_request
 import config_noIoT as config_noIoT
 
-web_request.send_request(config_noIoT.data, config_noIoT.web_url, config_noIoT.api_key)
+request = web_resquest()
+
+request.send_request(config_noIoT.data, config_noIoT.web_url, config_noIoT.api_key)
