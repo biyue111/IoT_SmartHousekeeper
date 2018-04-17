a90a4e9303e39c542e9f17aa02a14c435e23a50b
BI Yue <biyue111@126.com>
2018-04-17 21:46:32 +0800

    Update web_requests.py

--- broker/web_requests.py
+++ broker/web_requests.py
@@ -12,8 +12,6 @@ class web_request:
 
                 body = str.encode(json.dumps(data))
 
-                #url = 'https://ussouthcentral.services.azureml.net/workspaces/0a46720be6f3493bb4c2c05f05e3e5c4/services/27176938495a45368fbb33bb7f26e1e3/execute?api-version=2.0&details=true'
-                #api_key = 'yuhAKrw329F26sq9vXkc6JPt2kpvW0jNZYocNdHL32z3a8yM1DlCyUcbT06FkllVND2PtghZWOucvu/1r5kgYw==' # Replace this with the API key for the web service
                 headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
 
                 req = urllib2.Request(url, body, headers) 
