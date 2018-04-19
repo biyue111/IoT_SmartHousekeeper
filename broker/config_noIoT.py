4c6c0629eed9eff12e24d0fbb11cb120d5473864
BI Yue <biyue111@126.com>
2018-04-19 06:44:39 +0000

    Update config_noIoT.py

--- broker/config_noIoT.py
+++ broker/config_noIoT.py
@@ -17,10 +17,8 @@ user_input = { "ColumnNames": ["sleepStatus", "joyStatus" ],
                     "Values": [ [ "value", "value"], ] }
 
 activators_state = { "ColumnNames": ["ledStatus", "coffeeStatus", "curtainStatus"],
-                    "Values": [ [ "0", "value" , "0"], ] }
+                    "Values": [ [ "0", "0" , "0"], ] }
 activators_lock_timer = { "ColumnNames": ["ledStatus", "coffeeStatus", "curtainStatus"],
                     "Values": [ [ 0, 0 , 0], ] }
-lock_time = 100
+lock_time = 10
 
-web_url = 'https://ussouthcentral.services.azureml.net/workspaces/0a46720be6f3493bb4c2c05f05e3e5c4/services/27176938495a45368fbb33bb7f26e1e3/execute?api-version=2.0&details=true'
-api_key = 'yuhAKrw329F26sq9vXkc6JPt2kpvW0jNZYocNdHL32z3a8yM1DlCyUcbT06FkllVND2PtghZWOucvu/1r5kgYw=='
