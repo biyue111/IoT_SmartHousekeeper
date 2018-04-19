f2db730aacd1f060662c926dff51a3358bb08715
biyue111 <biyue111@126.com>
2018-04-19 20:53:39 +0800

    fix bug

--- broker/audiovoiceDetect.py
+++ broker/audiovoiceDetect.py
@@ -10,10 +10,11 @@ import os
 import socket
 from keyWordExtractUpdate import findWord
 import server_config as server_config
+import keys as keys
   
-URL = "http://api.xfyun.cn/v1/service/v1/iat"  
-APPID = "xxx"  
-API_KEY = "xxx"	 
+#URL = "http://api.xfyun.cn/v1/service/v1/iat"  
+#APPID = "xxx"  
+#API_KEY = "xxx"	 
   
 def getHeader():  
 	curTime = str(int(time.time()))	 
@@ -21,12 +22,12 @@ def getHeader():
 	paramBase64 = base64.b64encode(param)  
   
 	m2 = hashlib.md5()	
-	m2.update(API_KEY + curTime + paramBase64)	
+	m2.update(keys.API_KEY + curTime + paramBase64)	
 	checkSum = m2.hexdigest()  
 	header ={  
 		'X-CurTime':curTime,  
 		'X-Param':paramBase64,	
-		'X-Appid':APPID,  
+		'X-Appid':keys.APPID,  
 		'X-CheckSum':checkSum,	
 		'Content-Type':'application/x-www-form-urlencoded; charset=utf-8',	
 	}  
@@ -47,7 +48,7 @@ def main():
 		base64_audio = base64.b64encode(file_content)  
 		body = urllib.urlencode({'audio': base64_audio})  
 	  
-		r = requests.post(URL,headers=getHeader(),data=body)  
+		r = requests.post(keys.URL,headers=getHeader(),data=body)  
 		result = json.loads(r.content)	
 	  
 		if result["code"] == "0":  
